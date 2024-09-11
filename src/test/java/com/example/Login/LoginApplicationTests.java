cf0a578dee1d704450b892b49af8a1c2e297a5fa
edivyasree <edivyasree@gmail.com>
2024-09-11 13:11:21 +0530

    login

---
+++ src/test/java/com/example/Login/LoginApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.Login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class LoginApplication
+{
+public static void main(String[] args) {
+	SpringApplication.run(LoginApplication.class, args);
+}
+
+}
+
